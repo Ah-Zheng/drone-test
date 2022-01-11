0820248646bf0678f5f0ad19bb30bd2f1b5ce64a
Jerry092383 <jerry092383@mail.chungyo.net>
2022-01-11 12:57:45 +0800

    add text

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
     <div id="app">
         <img alt="Vue logo" src="./assets/logo.png" />
         <HelloWorld msg="Welcome to Your Vue.js App" />
-        <h1>Hello Jerry Cool qwe</h1>
+        <h1>Hello Jerry Cool</h1>
     </div>
 </template>
 
